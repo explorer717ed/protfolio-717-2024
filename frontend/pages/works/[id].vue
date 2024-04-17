61e501ccb0104a4cbb42b460ef0c79712c0c473b
717 <explorer717ed@gmail.com>
2024-04-17 17:45:01 +1200

    feat: front work mobile 1

--- frontend/pages/works/[id].vue
+++ frontend/pages/works/[id].vue
@@ -1,5 +1,32 @@
 <template>
-<h1>Works</h1>
+
+<section class="brief_section bg-dot">
+  <div class="breadcrumb">
+    <span class="breadcrumb_link">
+      <NuxtLink to="/">Home</NuxtLink>
+    </span>
+    <span class="breadcrumb_divider">/</span>
+    <span class="breadcrumb_link">Works</span>
+    <span class="breadcrumb_divider">/</span>
+    <span class="breadcrumb_link">Kronos Research</span>
+  </div>
+
+  <div class="brief_card">
+    
+    <button @click="goBack" class="brief_back_btn"><Icon title="Go back" name="material-symbols-light:arrow-left-alt"/></button>
+    <h1>Kronos Research</h1>
+    <p class="brief_feature">
+      > Technics: Three.js, Nuxt 3 <br>
+      > Year: 2024 <br>
+      > Scope: Frontend <br>
+    </p>
+    <p class="brief_desc">
+      A freelance project implementing Three.js. A freelance project implementing Three.js. A freelance project implementing Three.js. A freelance project implementing Three.js. A freelance project implementing Three.js. A freelance project implementing Three.js. A freelance project implementing Three.js.
+    </p>
+
+    <p class="brief_tags">#frontend #vue3 #nuxt3 #threejs</p>
+  </div>
+</section>
 </template>
 
 <script lang="ts" setup>
@@ -8,6 +35,10 @@ useHead({
   title: 'The Work Name'
 })
 
+const router = useRouter()
+const goBack = () => {
+  router.go(-1)
+}
 </script>
 
 <style lang="scss" scoped>
